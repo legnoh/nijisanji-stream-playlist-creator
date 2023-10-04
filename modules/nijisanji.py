ba869f8f9ba1fa68ad3212cbbe7e90ed706a3da1
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-10-05 07:05:59 +0900

    [skip ci] test

--- modules/nijisanji.py
+++ modules/nijisanji.py
@@ -24,7 +24,10 @@ def get_streams(subscription_channels, lang=None, archive_hours=12, wait_minutes
     pre_nominated_movies = []
     youtube_video_ids = []
 
-    favorite_keywords = ['雑談', '朝', '昼', '凸', 'ラジオ', '酒', '晩酌', 'らじ', 'RADIO', '料理', '掃除', '作業', '勉強']
+    favorite_keywords = ['雑談', 'ざつだん',
+                         '朝', '昼', '夕', '晩', '夜',
+                         '凸', '酒', '料理', '掃除', '作業', '勉強',
+                         'らじ', 'ラジ', 'RADIO']
     filter_keyword = '歌|メン限|メンバー限定|MEMBER'
     common_words = []
     words = []
